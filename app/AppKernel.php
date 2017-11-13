ab821cfeae67e839c5112ef807e67e6384dfaf28
Eric Pinxteren <epinxteren@ibuildings.nl>
2017-11-13 16:56:45 +0100

    Add gssp bundle to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,6 +13,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \Surfnet\GsspBundle\GsspBundle(),
             new AppBundle\AppBundle(),
         ];
 
